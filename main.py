1c9710249d52bb5878d227aed018514610834e30
piyushh-11 <115842755+piyushh-11@users.noreply.github.com>
2024-06-11 18:54:28 -0500

    open webcam with opencv

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Display the resulting frame
+    cv2.imshow('Webcam Feed', frame)
+
+    # Break the loop on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+cap.release()
+cv2.destroyAllWindows()
